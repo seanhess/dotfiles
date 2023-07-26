850f0242b003d256520a3ad8522b6302b834d807
Sean Hess <seanhess@gmail.com>
2023-07-26 11:29:58 -0600

    custom auto save on buffer leave only

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,3 +18,13 @@ vim.api.nvim_set_keymap("n", "Q", "<Nop>", { noremap = true, silent = true })
 vim.opt.expandtab = true -- Use spaces instead of tabs
 vim.opt.shiftwidth = 4 -- Size of an indent
 vim.opt.tabstop = 4 -- Number of spaces tabs count for
+
+-- save on exit buffer
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
+  callback = function()
+    if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
+      vim.api.nvim_command("update")
+
+    end
+  end,
+})
