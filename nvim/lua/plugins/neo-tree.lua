28d38d39a0401be9915a28d4c93fab9793ff5568
Sean Hess <seanhess@gmail.com>
2023-08-12 04:43:59 -0600

    disabled neo-tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,9 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 return {
+  -- WARNING: this deletes entire directories. Do not enable!
   "nvim-neo-tree/neo-tree.nvim",
+  enabled = false,
+
   opts = {
     window = {
       position = "float",
