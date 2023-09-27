36fdc114d67677a95c538a90a9950de75230b304
Sean Hess <seanhess@gmail.com>
2023-09-27 14:33:07 -0600

    disabled lsp-config to fix autoformat in haskell

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,22 +1,22 @@
-local function get_python_path(workspace)
-  local util = require("lspconfig/util")
-  local path = util.path
-  -- Use activated virtualenv.
-  if vim.env.VIRTUAL_ENV then
-    return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
-  end
-
-  -- Find and use virtualenv in workspace directory.
-  for _, pattern in ipairs({ "*", ".*" }) do
-    local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
-    if match ~= "" then
-      return path.join(path.dirname(match), "bin", "python")
-    end
-  end
-
-  -- Fallback to system Python.
-  return vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
-end
+-- local function get_python_path(workspace)
+--   local util = require("lspconfig/util")
+--   local path = util.path
+--   -- Use activated virtualenv.
+--   if vim.env.VIRTUAL_ENV then
+--     return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+--   end
+--
+--   -- Find and use virtualenv in workspace directory.
+--   for _, pattern in ipairs({ "*", ".*" }) do
+--     local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
+--     if match ~= "" then
+--       return path.join(path.dirname(match), "bin", "python")
+--     end
+--   end
+--
+--   -- Fallback to system Python.
+--   return vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
+-- end
 
 return {
   -- add pyright to lspconfig
@@ -32,16 +32,25 @@ return {
         },
       },
     },
+  }
 
-    config = function()
-      require("lspconfig").pyright.setup({
-        before_init = function(_, config)
-          local python_path = get_python_path(config.root_dir)
-          config.settings.python.pythonPath = python_path
-          vim.g.python_host_prog = python_path
-          vim.g.python3_host_prog = python_path
-        end,
-      })
-    end,
-  },
+  -- WARNING: overriding config disables haskell auto-format for some reason
+  --   config = function(opts)
+  --     local lsp = require('lspconfig')
+  --     lsp.hls.setup({
+  --       settings {
+  --         formattingProvider = "fourmolu"
+  --       }
+  --     })
+  --     lsp.pyright.setup({
+  --       before_init = function(_, config)
+  --         local python_path = get_python_path(config.root_dir)
+  --         config.settings.python.pythonPath = python_path
+  --         vim.g.python_host_prog = python_path
+  --         vim.g.python3_host_prog = python_path
+  --       end,
+  --     })
+  --   end,
+  -- },
 }
+-- return {}
